7fb0a086b7b18fa6614d8deec6c9b015e1a07d97
Osagie Itua <osagie@liquidhq.co>
2023-03-20 22:44:45 +0100

    Update  Dark Mode

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
