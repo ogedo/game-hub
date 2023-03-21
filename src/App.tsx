defd93c26b29c565a328b42b1e89efde25c3a17a
Osagie Itua <osagie@liquidhq.co>
2023-03-21 15:10:22 +0100

    Display genres

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX={5}>
           <GenreList />
         </GridItem>
       </Show>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,18 +1,29 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import React from "react";
 import useData from "../hooks/useData";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data, error, isLoading } = useGenres();
 
   return (
-    <ul>
+    <List>
       {isLoading && <li>Loading...</li>}
       {data.map((data) => (
-        <li key={data.id}>{data.name}</li>
+        <ListItem key={data.id}>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(data.image_background)}
+            />
+            <Text>{data.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
       {error && <li>Error: {error}</li>}
-    </ul>
+    </List>
   );
 };
 
