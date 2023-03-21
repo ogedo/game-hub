0a6e230566063add16d6d8f9a8893a30d3d1b931
Osagie Itua <osagie@liquidhq.co>
2023-03-21 01:49:56 +0100

    Show loading skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,10 +1,15 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
   //state variables for the game object
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = [
+    1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+    23, 24, 25, 26,
+  ];
 
   return (
     <>
@@ -14,6 +19,8 @@ const GameGrid = () => {
         padding="10x"
         spacing={10}
       >
+        {isLoading &&
+          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,27 +24,35 @@ export interface Game {
   
 const useGames = () => {
 
-
-
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [isLoading, setLoading] = useState(false);
 
   //send the request to the server
   useEffect(() => {
         //handle cancellation request
     const controller = new AbortController();
+
+    //set loading to true before sending the request
+    setLoading(true);
+    
     apiClient
       .get<FetchGamesResponse>("/games", {signal: controller.signal})
-      .then((res) => setGames(res.data.results))
+      .then((res) =>  {
+        setGames(res.data.results);
+    //set loading to true before sending the request
+         setLoading(false);
+        })
       .catch((err) => {
         if(err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
       return ()=> controller.abort();
   }, []);
 
-  return {games, error}
+  return {games, error, isLoading}
 
 }
 
