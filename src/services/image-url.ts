54d2fe2fdedc59ff02b33b5ff5c3c30208d35611
Osagie Itua <osagie@liquidhq.co>
2023-03-25 17:43:42 +0100

    Handle games with no images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,7 @@
+import noImage from "../assets/mo-image-paceholdder.webp";
 
 const getCroppedImageUrl = (url: string) => {
-  if(!url) return "";
+  if(!url) return "../assets/images/";
     const target = 'media/';
     const index = url.indexOf(target) + target.length;
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
