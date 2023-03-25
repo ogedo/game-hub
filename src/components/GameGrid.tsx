e50e1fd77b0625aa8649fa930ab8a30e93191199
Osagie Itua <osagie@liquidhq.co>
2023-03-25 15:06:29 +0100

    Built platform selector

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,10 +12,7 @@ interface Props {
 const GameGrid = ({ selectedGenre }: Props) => {
   //state variables for the game object
   const { data, error, isLoading } = useGames(selectedGenre);
-  const skeletons = [
-    1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-    23, 24, 25, 26,
-  ];
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
